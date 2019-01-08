cdd8b1e8f80d6fea46bbfb4dced12fcbc855bc71
yg <17665448577@163.com>
2019-01-08 10:35:44 +0800

    akka tell

--- src/main/java/com/akkademo/actor/AskDemoArticleParser.java
+++ src/main/java/com/akkademo/actor/AskDemoArticleParser.java
@@ -1,12 +1,11 @@
 package com.akkademo.actor;
 
-import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
+import akka.actor.*;
 import akka.http.scaladsl.model.HttpResponse;
 import akka.japi.pf.ReceiveBuilder;
 import akka.util.Timeout;
 import com.akkademo.messages.GetRequest;
+import com.akkademo.messages.SetRequest;
 import com.akkademo.service.ArticleBody;
 import com.akkademo.service.ParseArticle;
 import com.akkademo.service.ParseHtmlArticle;
@@ -15,6 +14,7 @@ import scala.runtime.BoxedUnit;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeoutException;
 
 import static akka.pattern.Patterns.ask;
 import static scala.compat.java8.FutureConverters.toJava;
@@ -37,6 +37,7 @@ public class AskDemoArticleParser extends AbstractActor {
         this.timeout = timeout;
     }
 
+    //ask 会返回future(CompletionStage)
     @Override
     public PartialFunction<Object, BoxedUnit> receive() {
         return ReceiveBuilder.match(ParseArticle.class, msg -> {
@@ -58,5 +59,39 @@ public class AskDemoArticleParser extends AbstractActor {
                 return null;
             });
         }).build();
+    }
+
+    //tell(更简单、高效)优先于ask
+   /* @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder.match(ParseArticle.class, msg -> {
+            ActorRef extraActor = buildExtraActor(sender(), msg.url);
+            cacheActor.tell(new GetRequest(msg.url), extraActor);
+            httpClienActor.tell(msg.url, extraActor);
+            context().system().scheduler().scheduleOnce(timeout.duration(), extraActor, "timeout", context().system().dispatcher(), ActorRef.noSender());//超时检查自身
+        }).build();
+    }*/
+
+    private ActorRef buildExtraActor(ActorRef senderRef, String url) {
+        class MyActor extends AbstractActor {
+            public MyActor() {
+                receive(
+                        ReceiveBuilder.matchEquals(
+                                String.class, x -> x.equals("timeout"), x -> {
+                                    senderRef.tell(new Status.Failure(new TimeoutException("timeout!")), self());
+                                    context().stop(self());
+                                }).match(HttpResponse.class, httpResponse -> articleParseActor.tell(new ParseHtmlArticle(url, httpResponse.entity()), self())
+                        ).match(String.class, body -> {
+                            senderRef.tell(body, self());
+                            context().stop(self());
+                        }).match(ArticleBody.class, articleBody -> {
+                            cacheActor.tell(new SetRequest(articleBody.body, self()), self());
+                            context().stop(self());
+                        }).matchAny(t -> System.err.println("ignoring msg: " + t.getClass())
+                        ).build()
+                );
+            }
+        }
+        return context().actorOf(Props.create(MyActor::new));
     }
 }
